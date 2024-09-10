b78438ac0657596f380829656a6dbc0bed0ae98b
백지연 <qorwldus8283@gmail.com>
2024-09-10 16:56:23 +0900

    오류처리

--- ex/ex10_calc.py
+++ ex/ex10_calc.py
@@ -1,3 +1,10 @@
+# 리스트, 튜플, 딕셔너리, set
+# bool()
+# datetime
+# random
+# 함수(def, 매개변수, 반환)
+
+
 # hello() : '어서오세요'를 출력하는 함수
 def hello() :
     print('어서오세요')
@@ -13,7 +20,7 @@ hello('박서현',13)
 # 매개변수의 이름으로 값을 넣는 방법
 hello(age = 13, name = '박서현')
 
-print('=' * 30)
+print('=' * 30) # -> 칸 나눠주는 역할 ===============
 # gugudan(dan) : 단에 해당하는 구구단을 출력
 def gugudan(dan) :
     for i in range(1,10) :
@@ -33,6 +40,7 @@ def test() :
 print('=' *30)
 # 함수 만들기
 # 주민등록번호를 입력받아서 성별을 반환하는 함수
+# getGender()
 name, age = ['박서현' , 13]
 def getGender(ssn) :
     ssn = str(ssn)
@@ -94,10 +102,46 @@ a,b,c =  getInfo('박서현','1111114444444')
 print(f'이름 : {a}, 나이 : {age}, 성별 : {c}')
 
 
-# getGender()
 
 print("=" *30)
+# 계산기 - 입력이 잘못된경우 0을 반환
+def cacl(operator, num1 , num2) :
+    # operator : +=*/%
+    if operator in ['+','-','*','/','%'] :
+        return 0
+    # .isnumeric() : 문자열이 가지고 있는 함수
+    # 문자열인 경우에만 사용이가능
+    # 타입이 다른 경우 사용 할 수 없음
+    # AttributeError : 'int' object has no attribute ' isnumeric'
+    # 함수에 괄호를 붙이지 않았을때
+    # <bulit-in method isnumeric of str object at 0x0000026C06345050>
+    print('=================', str(num1).isnumeric())
+    if operator == '+' :
+        return num1 + num2
+    elif operator == '-' :
+        return num1 - num2
+    elif operator == '*' :
+        return num1 * num2
+    elif operator == '/' :
+        return num1 / num2
+    elif operator == '%' :
+        return num1 % num2
 
+print('cacl(+,10,100)',cacl('+', 10,100))       
+
+# 평균을 구해 봅시다
+def avg(*numbers)  :
+    print('numbers', numbers)
+    # return sum(numbers)/len(numbers)
+    sum = 0
+    for num in numbers :
+        sum += num
+        return sum/len(numbers)
+
+print('avg : ',avg(1,2,3,4,5,6,7,8,9,10))
+
+
+print("=" *30)
 # 함수를 만들어 봅시다
 # 함수명 : calc(operator, *args)
 # operator : 연산자

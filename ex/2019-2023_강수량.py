9428488e8a9332a76975ace0b10fd3e089068f4e
백지연 <qorwldus8283@gmail.com>
2024-09-12 17:00:46 +0900

    객체

--- ex/2019-2023_강수량.py
+++
@@ -1,5 +0,0 @@
-# 한글깨짐 해결
-# 맷플롭에서 사용하는 폰트를 한글폰트로 변경
-from matplotlib import font_manager, rc, pyplot as plt
-font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
-rc('font', family=font_name)

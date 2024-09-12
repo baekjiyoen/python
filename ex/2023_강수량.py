2f4750b3bd2ab76d9083415583b25be923ed6fcd
백지연 <qorwldus8283@gmail.com>
2024-09-12 09:23:04 +0900

    2023 강수량

--- ex/2023_강수량.py
+++ ex/2023_강수량.py
@@ -31,7 +31,7 @@ import matplotlib.pyplot as plt
 
 # 한글깨짐 해결
 # 맷플롭에서 사용하는 폰트를 한글폰트로 변경
-from matplotlib import font_manager, rc
+from matplotlib import font_manager, rc , pyplot as plt
 font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
 rc('font', family=font_name)
 
